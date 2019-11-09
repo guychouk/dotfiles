1b046aa16f59b8565326d7818b3cfc20806a308e
cudacoder <10582405+cudacoder@users.noreply.github.com>
2019-11-09 23:43:12 +0200

    Refactor init.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,17 +6,19 @@
  '(company-idle-delay 0.2)
  '(custom-safe-themes
    (quote
-    ("1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" default)))
+    ("f357d72451c46d51219c3afd21bb397a33cb059e21db1f4adeffe5b8a9093537" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" default)))
  '(dap-mode 1 nil (dap-mode))
  '(dap-ui-mode 1 nil (dap-ui))
+ '(evil-collection-setup-debugger-keys t)
  '(evil-collection-setup-minibuffer t)
- '(helm-ag-fuzzy-match t)
+ '(exec-path-from-shell-check-startup-files nil)
+ '(helm-ag-fuzzy-match t t)
  '(helm-completion-style (quote emacs))
- '(neo-window-position (quote right))
- '(neo-window-width 45)
+ '(neo-window-position (quote right) t)
+ '(neo-window-width 45 t)
  '(package-selected-packages
    (quote
-    (indium buffer-move wakatime-mode shackle helm-ag evil-vimish-fold flycheck evil-org evil-goggles evil-commentary evil-escape evil-matchit evil-anzu evil-collection org-plus-contrib dap-node dap-mode tern prettier-js spaceline general web-beautify web-mode emmet-mode json-mode yaml-mode php-mode powershell helm-projectile helm projectile evil-numbers evil-surround evil-mc evil company undo-tree diff-hl magit neotree exec-path-from-shell use-package-ensure-system-package gruvbox-theme diminish use-package)))
+    (company-lsp git-gutter night-owl-theme gist paredit password-store indium buffer-move wakatime-mode shackle helm-ag evil-vimish-fold flycheck evil-org evil-goggles evil-commentary evil-escape evil-matchit evil-anzu evil-collection org-plus-contrib dap-node dap-mode tern prettier-js spaceline general web-beautify web-mode emmet-mode json-mode yaml-mode php-mode powershell helm-projectile helm projectile evil-numbers evil-surround evil-mc evil company undo-tree diff-hl magit neotree exec-path-from-shell use-package-ensure-system-package gruvbox-theme diminish use-package)))
  '(powerline-default-separator (quote arrow-fade))
  '(powerline-height 20)
  '(projectile-completion-system (quote helm))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,28 +1,36 @@
-					;  use-package Setup  ;
-					; ;;;;;;;;;;;;;;;;;;; ;
-(eval-and-compile
-  (setq load-prefer-newer t
-        package--init-file-ensured t
-        package-enable-at-startup nil))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SETUP ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Initialize package
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (eval-when-compile
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package)
-    (package-install 'diminish))
-  (setq use-package-always-ensure t)
-  (setq use-package-expand-minimally t)
+  (defvar use-package-always-ensure t) ; Implicit :ensure for all packages
   (require 'use-package))
+(require 'diminish) ; Helps suppress minor modes indications in mode line.
+(require 'bind-key) ; Adds option of binding keys to packages.
 
-(require 'diminish)
+;; Add a :use-package-ensure-system symbol which enables
+;; installation of *system* packages before installing a package.
+(use-package use-package-ensure-system-package)
 
-					;  General Settings  ;
-					; ;;;;;;;;;;;;;;;;;; ;
+;; Only add this MacOS to fix the $PATH environment variable
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :custom
+  (exec-path-from-shell-check-startup-files nil)
+  :config
+  (exec-path-from-shell-initialize))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; GENERAL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Quiet Startup
 (setq inhibit-startup-screen t)
@@ -63,11 +71,11 @@
     (server-start))
 
 ;; Enable Winner mode
-;; (Enabled undo redo for window configurations)
+;; (Enable undo/redo for window configurations)
 (winner-mode 1)
 
-					;  GUI & Theme  ;
-					; ;;;;;;;;;;;;; ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; GUI ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Remove menu-bar
 (menu-bar-mode -1)
@@ -82,73 +90,54 @@
 (cond
  ((string-equal system-type "windows-nt")
   (progn
-    (set-default-font "Liberation Mono 10")))
+    (add-to-list 'default-frame-alist
+                 '(font . "Liberation Mono 10"))))
  ((string-equal system-type "darwin")
   (progn
-    (set-default-font "Iosevka 14"))))
+    (add-to-list 'default-frame-alist
+                 '(font . "Iosevka 14")))))
 
-;; Set spacemacs-dark theme
-(use-package gruvbox-theme
+;; Set theme
+(use-package night-owl-theme
   :if (display-graphic-p)
   :init
-  (load-theme 'gruvbox-dark-soft))
+  (load-theme 'night-owl))
 
-					;     Functions     ;
-					; ;;;;;;;;;;;;;;;;; ;
-
-;; Override projectile's get external command
-(defun projectile-get-ext-command() ""
-       (concat "D:\\Drive\\bin\\es.exe -r " 
-	       (concat (replace-regexp-in-string "/" "\\\\" default-directory t t) 
-		       ".+[^\\\\]\\.[^\\\\]+$ | tr '\\n' '\\0'")))
-
-					;  Packages Config  ;
-					; ;;;;;;;;;;;;;;;;; ;
-
-;; Add a :use-package-ensure-system symbol which enables
-;; installation of *system* packages before installing a package.
-(use-package use-package-ensure-system-package)
-
-;; Only add this MacOS to fix the $PATH environment variable
-(use-package exec-path-from-shell
-  :if (memq window-system '(mac ns))
+;; Sets a constant location and size for the helm split buffer
+(use-package shackle
   :config
-  (exec-path-from-shell-initialize))
+  (setq helm-display-function #'pop-to-buffer)
+  (setq shackle-rules '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :ratio 0.46)))
+  (shackle-mode))
 
-;; Using indium for the meantime, here for legacy purposes
-;;
-;; (use-package dap-mode
-;;   :custom
-;;   (dap-mode 1)
-;;   (dap-ui-mode 1)
-;;   :bind (("S-<f8>" . dap-breakpoint-toggle)
-;;          ("<f5>" . dap-debug)
-;;          ("<f7>" . dap-step-in)
-;;          ("<f6>" . dap-step-out)
-;;          ("<f9>" . dap-continue)
-;;          ("<f8>" . dap-next))
-;;   :config
-;;   (require 'dap-node)
-;;   (dap-register-debug-template "Users API"
-;;                                (list :type "node"
-;;                                      :program ""
-;;                                      :port "30001"
-;;                                      :request "attach"
-;;                                      :remoteRoot "/app/src"
-;;                                      :localRoot "/Users/guyvalariola/Projects/box/projects/users/src"))
-;;   (require 'dap-chrome)
-;;   (dap-register-debug-template "SDK"
-;;                                (list
-;;                                 :name "SDK"
-;;                                 :type "chrome"
-;;                                 :cwd nil
-;;                                 :mode "url"
-;;                                 :request "launch"
-;;                                 :url "https://sdk.apester.local.com/"
-;;                                 :webRoot "/Users/guyvalariola/Projects/box/projects/sdk/src")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; FUNCTIONS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun my-js2-mode-setup nil
+  "My js2-mode setup."
+  (flycheck-mode t)
+  (setq js-indent-level 2)
+  (when (executable-find "eslint")
+    (flycheck-select-checker 'javascript-eslint)))
+
+(defun disable-fylcheck-in-org-src-block ()
+  (setq-local flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+
+(defun my-kill-thing-at-point (thing)
+  "Kill the `thing-at-point' for the specified kind of THING."
+  (let ((bounds (bounds-of-thing-at-point thing)))
+    (if bounds
+        (kill-region (car bounds) (cdr bounds))
+      (error "No %s at point" thing))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; PACKAGES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Diminish Emacs modes indications
+(diminish 'undo-tree)
+(diminish 'eldoc-mode)
 
 (use-package neotree
-  :defer t
   :bind ("M-1" . neotree-toggle)
   :custom
   (neo-window-position 'right)
@@ -156,45 +145,9 @@
 
 (use-package magit)
 
-(use-package diff-hl
-  :init
-  (global-diff-hl-mode)
-  :hook
-  (magit-post-refresh . diff-hl-magit-post-refresh)
+(use-package git-gutter
   :config
-  (diff-hl-margin-mode)
-  (diff-hl-flydiff-mode))
-
-(use-package eldoc
-  :defer t
-  :diminish)
-
-(use-package undo-tree
-  :defer t
-  :diminish)
-
-(use-package spaceline
-  :after evil
-  :custom
-  (powerline-height 20)
-  (powerline-default-separator 'arrow-fade)
-  (spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
-  :config
-  (unless (package-installed-p 'spaceline) (spaceline-install))
-  (spaceline-spacemacs-theme)
-  (spaceline-toggle-buffer-encoding-abbrev-off)
-  (spaceline-toggle-line-column-off)
-  (spaceline-toggle-buffer-position-off)
-  (spaceline-toggle-hud-off)
-  (spaceline-toggle-buffer-size-off)
-  (spaceline-helm-mode))
-
-(use-package company
-  :diminish
-  :custom
-  (company-idle-delay 0.2)
-  :config
-  (global-company-mode 1))
+  (global-git-gutter-mode +1))
 
 (use-package wakatime-mode
   :diminish
@@ -202,23 +155,24 @@
   :config
   (global-wakatime-mode))
 
-(defun my-js2-mode-setup nil
-    "My js2-mode setup."
-  (flycheck-mode t)
-  (setq js-indent-level 2)
-  (when (executable-find "eslint")
-    (flycheck-select-checker 'javascript-eslint)))
-
 (use-package flycheck
   :diminish
   :init (global-flycheck-mode)
   :ensure-system-package (eslint . "npm i -g eslint")
+  :hook
+  (org-src-mode . disable-fylcheck-in-org-src-block)
+  (emacs-lisp-mode . disable-fylcheck-in-org-src-block)
+  :custom
+  (flycheck-indication-mode nil)
   :config
   (setq js2-include-node-externs t)
   (setq-default flycheck-disabled-checkers
-		(append flycheck-disabled-checkers
-			'(javascript-jshint)))
+                (append flycheck-disabled-checkers
+                        '(javascript-jshint)))
   (add-hook 'js2-mode-hook 'my-js2-mode-setup))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; EVIL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package evil
   :init
@@ -241,13 +195,11 @@
   (evil-set-initial-state 'magit-blame-mode 'emacs)
   (evil-set-initial-state 'git-blame-mode 'emacs)
   (evil-set-initial-state 'indium-repl-mode 'emacs)
+  (evil-set-initial-state 'indium-debugger-mode 'emacs)
   (evil-set-initial-state 'dap-ui-breakpoints-ui-list-mode 'emacs)
   (add-hook 'git-commit-mode-hook 'evil-emacs-state)
   (evil-set-initial-state 'magit-log-edit-mode 'insert)
   (evil-mode 1))
-
-;; Best JS development plugin for Emacs
-(use-package indium)
 
 ;; Collection of evil keybindings for the parts of Emacs that Evil does not cover properly
 (use-package evil-collection
@@ -258,13 +210,12 @@
   :config
   (evil-collection-init))
 
-;; Port for Vim's anzu to show search status in status bar
-(use-package evil-anzu
-  :after evil)
-
-;; Match more than parentheses
-(use-package evil-matchit
-  :after evil)
+;; Enable increment / decrement of numbers with C-= and C-- similar to Vim
+(use-package evil-numbers
+  :config
+  (progn
+    (define-key evil-normal-state-map (kbd "C-=") 'evil-numbers/inc-at-pt)
+    (define-key evil-normal-state-map (kbd "C--") 'evil-numbers/dec-at-pt)))
 
 ;; Escape from many modes
 (use-package evil-escape
@@ -276,12 +227,9 @@
     (global-set-key (kbd "<escape>") 'evil-escape))
   :diminish evil-escape-mode)
 
-;; C-+ C-- to increase/decrease number like Vim's C-a C-x
-(use-package evil-numbers
-  :config
-  (progn
-    (define-key evil-normal-state-map (kbd "C-=") 'evil-numbers/inc-at-pt)
-    (define-key evil-normal-state-map (kbd "C--") 'evil-numbers/dec-at-pt)))
+;; Match more than parentheses
+(use-package evil-matchit
+  :after evil)
 
 ;; Emulate vim-surround package
 (use-package evil-surround
@@ -307,10 +255,10 @@
   (evil-goggles-use-diff-faces))
 
 ;; Setup evil multi-cursor
-(use-package evil-mc
-  :diminish
-  :requires evil
-  :config (global-evil-mc-mode 1))
+;; (use-package evil-mc
+;;   :diminish
+;;   :requires evil
+;;   :config (global-evil-mc-mode 1))
 
 (use-package evil-org
   :after org
@@ -328,20 +276,27 @@
   :config
   (evil-vimish-fold-mode 1))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ESSENTIALS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Index and search projects using standard means
 (use-package projectile
   :diminish
   :custom
-  (projectile-switch-project-action 'neotree-projectile-action)
   (projectile-enable-caching nil)
   (projectile-indexing-method 'alien))
 
-;; Sets a constant location and size for the helm split buffer
-(use-package shackle
+;; Auto complete engine for Emacs
+(use-package company
+  :diminish
   :config
-  (setq helm-display-function #'pop-to-buffer)
-  (setq shackle-rules '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :ratio 0.46)))
-  (shackle-mode))
+  (global-company-mode 1))
+
+;; LSP backend for Company
+(use-package company-lsp
+  :requires company
+  :config
+  (push 'company-lsp company-backends))
 
 ;; Helm <3
 (use-package helm
@@ -353,9 +308,6 @@
 ;; (similar behaviour to how VSCode uses <C-p> for traversing files in project)
 (use-package helm-projectile
   :after (helm projectile)
-  :bind (("C-S-P" . helm-projectile-switch-project)
-	 :map evil-normal-state-map
-	 ("C-p" . helm-projectile))
   :custom
   (projectile-completion-system 'helm)
   :config
@@ -367,42 +319,79 @@
   :after (helm projectile)
   :ensure-system-package (ag . "brew install ag")
   :bind (("C-S-f" . helm-projectile-ag)
-	 :map evil-normal-state-map
+         :map evil-normal-state-map
          ("C-f" . helm-ag-this-file))
   :custom
   (helm-ag-fuzzy-match t))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; MODES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Setup language modes
-(use-package js2-mode)
 (use-package php-mode)
-(use-package yaml-mode :mode "\\.yml\\'")
-(use-package tern
-  :hook (web-mode . tern-mode)
-  :ensure-system-package (tern . "npm i -g tern"))
-(use-package prettier-js
-  :hook (web-mode . prettier-js-mode)
-  :ensure-system-package (prettier . "npm i -g prettier"))
-(use-package emmet-mode
-  :hook (web-mode . emmet-mode)
-  :hook (css-mode . emmet-mode)
-  :hook (js2-mode . emmet-mode))
+(use-package js2-mode)
+(use-package yaml-mode)
+(use-package emmet-mode)
 (use-package web-mode
-  :after js2-mode
+  :after (js2-mode php-mode yaml-mode)
   :mode ("\\.js\\'" . js2-mode)
   :mode ("\\.jsx\\'" . js2-mode)
   :mode ("\\.php\\'" . php-mode)
   :mode ("\\.css\\'" . css-mode)
-  :mode ("\\.scss\\'" . scss-mode)
   :mode ("\\.html\\'" . web-mode)
-  :mode ("\\.json\\'" . web-mode))
+  :mode ("\\.json\\'" . web-mode)
+  :mode ("\\.scss\\'" . scss-mode)
+  :mode ("\\.yml\\'" . yaml-mode))
 
-;; Setup keybindings comfortably
+(use-package lsp-mode
+  :hook (js2-mode . lsp)
+  :ensure-system-package ((typescript . "npm i -g typescript")
+                          (tsserver . "npm i -g typescript-language-server")))
+
+(use-package indium)
+(use-package dap-mode
+  :custom
+  (dap-mode 1)
+  (dap-ui-mode 1)
+  :bind (("S-<f8>" . dap-breakpoint-toggle)
+         ("<f5>" . dap-debug)
+         ("<f7>" . dap-step-in)
+         ("<f6>" . dap-step-out)
+         ("<f9>" . dap-continue)
+         ("<f8>" . dap-next))
+  :config
+  (require 'dap-node)
+  (dap-register-debug-template "Users API"
+                               (list :type "node"
+                                     :program ""
+                                     :port "30001"
+                                     :request "attach"
+                                     :remoteRoot "/app/src"
+                                     :localRoot "/Users/guyvalariola/Projects/box/projects/users/src"))
+  (require 'dap-chrome)
+  (dap-register-debug-template "SDK" (list
+                                      :name "SDK"
+                                      :type "chrome"
+                                      :cwd nil
+                                      :mode "url"
+                                      :request "launch"
+                                      :url "https://sdk.apester.local.com/"
+                                      :webRoot "/Users/guyvalariola/Projects/box/projects/sdk/src")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; KEYBINDINGS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (use-package general
   :requires evil
   :config
   (general-evil-setup t)
   (general-define-key "M-x" 'helm-M-x)
+  (general-imap
+    "C-n" #'company-select-next
+    "C-p" #'company-select-previous
+    "C-j" #'company-complete-selection)
+  (general-nmap
+    "C-p" #'helm-projectile
+    "C-S-P" #' helm-projectile-switch-project)
   ;; add "A" and "I" shortcuts for adding MC at the end / beginning
   (general-vmap
     "A" #'evil-mc-make-cursor-in-visual-selection-end
@@ -424,4 +413,5 @@
     "ff" 'helm-find-files
     "pi" 'package-install
     "pd" 'package-delete
+    "z" (lambda() (interactive)(find-file "~/.zshrc"))
     "e" (lambda() (interactive)(find-file "~/.emacs.d/init.el"))))
