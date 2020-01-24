283906dbc35fc2b091bf5124cbd135c51d06de4e
cudacoder <10582405+cudacoder@users.noreply.github.com>
2020-01-24 23:05:05 +0200

    Move  logic from init.el to

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,9 +8,8 @@
 
 (push (expand-file-name "~/.emacs.d/lisp") load-path)
 
-(defvar *is-a-mac* (eq system-type 'darwin) "MacOS check.")
-(defvar best-gc-cons-threshold 4000000 "Recomended GC threshold taken from redguardtoo/emacs.d repo.")
-(defvar backup--file-name-handler-alist file-name-handler-alist)
+(defvar best-gc-cons-threshold 16777216 "Recomended GC threshold taken from DOOM Emacs (16mb).")
+(defvar backup--file-name-handler-alist file-name-handler-alist "Backup 'file-name-handler-alist'.")
 
 ;; See https://github.com/hlissner/doom-emacs/blob/develop/docs/faq.org#unset-file-name-handler-alist-temporarily
 (setq file-name-handler-alist nil)
@@ -28,82 +27,17 @@
   (run-at-time
    1 nil (lambda () (setq gc-cons-threshold best-gc-cons-threshold))))
 
+(add-hook 'minibuffer-setup-hook #'doom-defer-garbage-collection-h)
+(add-hook 'minibuffer-exit-hook #'doom-restore-garbage-collection-h)
+
 (defun require-init (pkg)
   "Load PKG by giving the `load` function an absolute path (faster)."
   (load (file-truename (format "~/.emacs.d/lisp/%s" pkg))))
 
-(add-hook 'minibuffer-setup-hook #'doom-defer-garbage-collection-h)
-(add-hook 'minibuffer-exit-hook #'doom-restore-garbage-collection-h)
-
-(setq package-selected-packages '(all-the-icons
-                                  cargo
-                                  company
-                                  company-lsp
-                                  counsel
-                                  counsel-projectile
-                                  dap-mode
-                                  diff-hl
-                                  diminish
-                                  doom-modeline
-                                  doom-themes
-                                  emmet-mode
-                                  evil
-                                  ;; evil-collection
-                                  evil-commentary
-                                  evil-escape
-                                  evil-goggles
-                                  evil-matchit
-                                  evil-numbers
-                                  evil-org
-                                  evil-surround
-                                  evil-vimish-fold
-                                  exec-path-from-shell
-                                  eyebrowse
-                                  flycheck
-                                  flycheck-rust
-                                  gist
-                                  gnuplot
-                                  hydra
-                                  ivy
-                                  js2-mode
-                                  json-mode
-                                  lsp-mode
-                                  magit
-                                  markdown-mode
-                                  nov
-                                  org-bullets
-                                  org-plus-contrib
-                                  php-mode
-                                  prodigy
-                                  projectile
-                                  rjsx-mode
-                                  rust-mode
-                                  swiper
-                                  typescript-mode
-                                  wakatime-mode
-                                  web-mode
-                                  yaml-mode
-                                  yasnippet
-                                  yasnippet-snippets))
-
-(setq package-enable-at-startup nil)
-(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-(package-install-selected-packages)
-
-;; Only use this if OS is Mac OSX to fix the $PATH environment variable
-(when *is-a-mac*
-  (package-install 'exec-path-from-shell)
-  (defvar exec-path-from-shell-check-startup-files nil)
-  (require 'exec-path-from-shell)
-  (exec-path-from-shell-initialize))
 
 (require-init 'base)
-(require-init 'base-gui)
+(require-init 'init-package)
+(require-init 'init-gui)
 (require-init 'init-evil)
 (require-init 'init-packages)
 (require-init 'init-utils)
@@ -114,7 +48,7 @@
 
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold 16777216 ; 16mb
+            (setq gc-cons-threshold best-gc-cons-threshold
                   gc-cons-percentage 0.1
                   file-name-handler-alist backup--file-name-handler-alist)))
 
---
+++ .emacs.d/lisp/init-package.el
@@ -0,0 +1,83 @@
+;;; init-package.el --- Package Setup
+
+;;; Commentary:
+;; Initialize Emac's `package` and
+;; install packagesif necessary.
+
+;;; Code:
+
+;; -*- coding: utf-8; lexical-binding: t; -*-
+
+
+(defvar *is-a-mac* (eq system-type 'darwin) "MacOS check.")
+
+(setq package-selected-packages '(all-the-icons
+                                  cargo
+                                  company
+                                  company-lsp
+                                  counsel
+                                  counsel-projectile
+                                  dap-mode
+                                  diff-hl
+                                  diminish
+                                  doom-modeline
+                                  doom-themes
+                                  emmet-mode
+                                  evil
+                                  ;; evil-collection
+                                  evil-commentary
+                                  evil-escape
+                                  evil-goggles
+                                  evil-matchit
+                                  evil-numbers
+                                  evil-org
+                                  evil-surround
+                                  evil-vimish-fold
+                                  exec-path-from-shell
+                                  eyebrowse
+                                  flycheck
+                                  flycheck-rust
+                                  gist
+                                  gnuplot
+                                  hydra
+                                  ivy
+                                  js2-mode
+                                  json-mode
+                                  lsp-mode
+                                  magit
+                                  markdown-mode
+                                  nov
+                                  org-bullets
+                                  org-plus-contrib
+                                  php-mode
+                                  prodigy
+                                  projectile
+                                  rjsx-mode
+                                  rust-mode
+                                  swiper
+                                  typescript-mode
+                                  wakatime-mode
+                                  web-mode
+                                  yaml-mode
+                                  yasnippet
+                                  yasnippet-snippets))
+
+(setq package-enable-at-startup nil)
+(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(package-install-selected-packages)
+
+;; Only use this if OS is Mac OSX to fix the $PATH environment variable
+(when *is-a-mac*
+  (package-install 'exec-path-from-shell)
+  (defvar exec-path-from-shell-check-startup-files nil)
+  (require 'exec-path-from-shell)
+  (exec-path-from-shell-initialize))
+
+(provide 'init-package)
+
+;;; init-package.el ends here
