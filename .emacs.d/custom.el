535b24620272358e93b6246b7928c051576231e1
cudacoder <10582405+cudacoder@users.noreply.github.com>
2020-04-06 01:43:27 +0300

    Post packages & DAP settings cleanup

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,23 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(dap-auto-show-output nil)
- '(exec-path-from-shell-check-startup-files nil)
  '(package-selected-packages
    (quote
-    (ag terraform-mode all-the-icons cargo company company-lsp company-restclient counsel counsel-projectile dap-mode diff-hl diminish doom-modeline doom-themes emmet-mode evil evil-collection evil-commentary evil-escape evil-goggles evil-matchit evil-numbers evil-org evil-surround evil-vimish-fold exec-path-from-shell eyebrowse flycheck flycheck-rust gist gnuplot hydra ivy js2-mode json-mode lsp-mode magit markdown-mode neotree nov org-bullets org-plus-contrib paredit php-mode prodigy projectile quelpa-use-package restclient rust-mode shackle swiper toml-mode typescript-mode use-package use-package-ensure-system-package wakatime-mode web-mode yaml-mode yasnippet yasnippet-snippets)))
- '(wakatime-cli-path "/usr/local/bin/wakatime")
- '(wakatime-python-bin nil))
+    (ace-window adoc-mode terraform-mode all-the-icons cargo company company-lsp counsel counsel-projectile dap-mode diff-hl diminish doom-modeline doom-themes emmet-mode evil evil-commentary evil-matchit evil-surround evil-vimish-fold exec-path-from-shell eyebrowse flycheck flycheck-rust gist gnuplot go-mode hydra ivy ivy-posframe js2-mode json-mode lsp-mode magit markdown-mode nov org-bullets org-plus-contrib php-mode prodigy projectile rust-mode swiper typescript-mode wakatime-mode web-mode yaml-mode yasnippet yasnippet-snippets))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(evil-goggles-change-face ((t (:inherit diff-removed))))
- '(evil-goggles-delete-face ((t (:inherit diff-removed))))
- '(evil-goggles-paste-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
- '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
- '(evil-goggles-yank-face ((t (:inherit diff-changed))))
- '(internal-border ((t (:background "black" :foreground "black")))))
+ )
