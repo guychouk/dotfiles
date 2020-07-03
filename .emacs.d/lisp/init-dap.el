9b4f025eaa16f3ee01ea1b330863b54471fd5d23
cudacoder <10582405+cudacoder@users.noreply.github.com>
2020-07-03 20:28:39 +0300

    Add functionality to init-dap.el & Add new scripts

--- .emacs.d/lisp/init-dap.el
+++ .emacs.d/lisp/init-dap.el
@@ -6,6 +6,43 @@
 
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
+(defun dap/eval-object-to-clipboard (expression)
+  "Eval EXPRESSION and save result to clipboard."
+  (interactive "sEval: ")
+  (let ((debug-session (dap--cur-active-session-or-die)))
+    (if-let ((active-frame-id (-some->> debug-session
+                                        dap--debug-session-active-frame
+                                        (gethash "id"))))
+        (dap--send-message
+         (dap--make-request "evaluate"
+                            (list :expression (concat "JSON.stringify(" expression ",null,2)")
+                                  :frameId active-frame-id))
+         (-lambda ((&hash "success" "message" "body"))
+           (with-temp-buffer
+             (insert (gethash "result" body))
+             (clipboard-kill-region (point-min) (point-max))))
+         debug-session)
+      (error "There is no stopped debug session"))))
+
+(defun dap/show-debug-windows (session)
+  "Show debug windows in SESSION."
+  (let ((lsp--cur-workspace (dap--debug-session-workspace session)))
+    (save-excursion
+      ;; display locals
+      (unless (my/window-visible dap-ui--locals-buffer)
+        (dap-ui-locals))
+      (unless (my/window-visible "*dap-ui-repl*")
+        (dap-ui-repl)))))
+
+(defun dap/hide-debug-windows (session)
+  "Hide debug windows when SESSION is terminated."
+  (unless (-filter 'dap--session-running (dap--get-sessions))
+    (and (get-buffer dap-ui--locals-buffer)
+         (kill-buffer dap-ui--locals-buffer))
+    (and (get-buffer "*dap-ui-repl*")
+         (kill-buffer "*dap-ui-repl*"))))
+
+
 (defun dap/show-locals (session)
   "Show *dap-locals* buffer after stopping on breakpoint in SESSION."
   (when (not (get-buffer-window "*dap-ui-locals*"))
@@ -37,7 +74,9 @@
 (define-key global-map (kbd "S-<f8>") 'dap-breakpoint-toggle)
 
 (add-hook 'dap-stopped-hook 'dap/show-locals)
+(add-hook 'dap-stopped-hook 'dap/show-debug-windows)
 (add-hook 'dap-terminated-hook 'dap/hide-locals)
+(add-hook 'dap-terminated-hook 'dap/hide-debug-windows)
 
 (provide 'init-dap)
 
