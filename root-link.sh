9bdabf55074e50de6a1258d503ce9853f4ba5005
guychouk <10582405+guychouk@users.noreply.github.com>
2024-12-09 12:53:47 +0100

    Add cache ttl to GPG & Clear cache on screen lock

---
+++ root-link.sh
@@ -0,0 +1,2 @@
+ln -s /Users/gv/dotfiles/agents/com.gv.clear-agent-caches.plist /Users/gv/Library/LaunchAgents/com.gv.clear-agent-caches.plist
+sudo ln -s /Users/gv/dotfiles/scripts/clear-agent-caches /usr/local/bin/clear-agent-caches
