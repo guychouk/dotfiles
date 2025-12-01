9f32edd1d6514330283a857bee953f63cf0208a5
guychouk <10582405+guychouk@users.noreply.github.com>
2025-12-01 10:29:57 +0100

    fish: add fish configuration

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,30 @@
+# Custom prompt with git branch and kubectl context
+function fish_prompt
+    # Set cursor to blinking underline
+    echo -ne '\e[3 q'
+
+    # Hostname
+    echo -ne '\e[38;5;8m'
+    echo -n (prompt_hostname)' '
+    echo -ne '\e[0m'
+
+    # Current directory (last 2 components)
+    echo -ne '\e[38;5;4m'
+    echo -n (prompt_pwd --dir-length=2)
+    echo -ne '\e[0m'
+
+    # Git branch
+    if git rev-parse HEAD >/dev/null 2>&1
+        set git_branch (git symbolic-ref --short HEAD 2>/dev/null)
+        if test -n "$git_branch"
+            echo -ne '\e[38;5;180m'
+            echo -n " $git_branch"
+            echo -ne '\e[0m'
+        end
+    end
+
+    # Lambda
+    echo -ne '\e[38;5;16m'
+    echo -n ' λ '
+    echo -ne '\e[0m'
+end
