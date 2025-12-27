f343eb071736aac8451731125715c58aa0cb54df
guychouk <10582405+guychouk@users.noreply.github.com>
2025-12-27 05:23:54 +0100

    zsh,fish,scripts: add proper snip functionality & pruning

---
+++ fish/functions/snip.fish
@@ -0,0 +1,25 @@
+function snip -d "Select and insert snippet into command line"
+    set -l snippets_file ~/dotfiles/SNIPPETS
+
+    if test "$argv[1]" = "add"
+        vim $snippets_file
+        return
+    end
+
+    # Use fzf to select a snippet - preview shows the command
+    set -l selected (
+        awk '/^#/ {desc=substr($0, 3); getline; cmd=$0; print desc "\t" cmd}' $snippets_file | \
+        fzf --delimiter='\t' --with-nth=1 \
+            --preview="echo {2..} | bat -l bash -p --color always" \
+            --preview-window=up:3:wrap \
+            --bind='ctrl-y:preview-up,ctrl-e:preview-down'
+    )
+
+    # If no selection, exit
+    test -z "$selected"; and return 1
+
+    # Extract command and insert into command line
+    set -l cmd (echo $selected | cut -f2-)
+    commandline -r $cmd
+    commandline -f repaint
+end
--- scripts/clear-gpg-cache.sh
+++ scripts/clear-gpg-cache.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 gpgconf --kill gpg-agent
 gpgconf --launch gpg-agent
