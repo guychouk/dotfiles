4278f5068661db44bcd8610505f82ba55d6ac016
guychouk <10582405+guychouk@users.noreply.github.com>
2026-02-01 14:36:30 +0100

    gpg,scripts: fix gpg-agent pinentry auth flow

--- link.sh
+++ link.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-# XDG configs - just directory names, will link to ~/.config/
+# XDG configs (links to ~/.config/)
 xdg_configs=(
   fish
   git
@@ -15,8 +15,8 @@ xdg_configs=(
   newsboat
 )
 
-# Legacy locations - format: "source:destination"
-legacy_links=(
+# Other locations - "source:destination"
+other_links=(
   "vim:$HOME/.vim"
   "ctags:$HOME/.ctags.d"
   "zsh/.zshrc:$HOME/.zshrc"
@@ -25,14 +25,13 @@ legacy_links=(
   "gnupg/gpg-agent.conf:$HOME/.gnupg/gpg-agent.conf"
   "curl/.curlrc:$HOME/.config/.curlrc"
   "scripts:$HOME/scripts"
-  "agents/com.gv.clear-gpg-cache.plist:$HOME/Library/LaunchAgents/com.gv.clear-gpg-cache.plist"
 )
 
 if [[ "$1" == "unlink" ]]; then
   for dir in "${xdg_configs[@]}"; do
     rm -f ~/.config/$dir
   done
-  for link in "${legacy_links[@]}"; do
+  for link in "${other_links[@]}"; do
     dest="${link#*:}"
     rm -f "$dest"
   done
@@ -41,7 +40,7 @@ else
     rm -f ~/.config/$dir
     ln -s "$PWD/$dir" ~/.config/$dir
   done
-  for link in "${legacy_links[@]}"; do
+  for link in "${other_links[@]}"; do
     src="${link%%:*}"
     dest="${link#*:}"
     rm -f "$dest"
--- scripts/clear-gpg-cache.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-gpgconf --kill gpg-agent
-gpgconf --launch gpg-agent
