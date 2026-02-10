3955ffd53418799d1500cd8935ea9a9b7f7ed3f9
guychouk <10582405+guychouk@users.noreply.github.com>
2026-02-10 18:20:29 +0100

    fish,kitty: organize fish functions and kitty pickers

--- fish/functions/fzf-docs-dir.fish
+++
@@ -1,3 +0,0 @@
-function fzf-docs-dir
-    find ~/Documents/ -type d -not -path '*/.git/*' -not -name '.git' | fzf --pointer="▶" | tr -d '\n' | pbcopy
-end
--- fish/functions/fzf-projects-file.fish
+++
@@ -1,3 +0,0 @@
-function fzf-projects-file
-    find ~/Projects -type f -not -path '*/.git/*' | fzf --pointer="▶" | tr -d '\n' | pbcopy
-end
