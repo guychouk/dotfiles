ba8e331200ef126731b829feca01e8e4a1b56f94
guychouk <10582405+guychouk@users.noreply.github.com>
2026-03-25 13:09:24 +0100

    fish: add support for PASSWORDS_STORE_DIR to pcp

--- fish/completions/pcp.fish
+++ fish/completions/pcp.fish
@@ -1,1 +1,1 @@
-complete -c pcp -f -a "(find ~/.password-store -name '*.gpg' | sed 's|.*/.password-store/||; s|\.gpg||')"
+complete -c pcp -f -a "(find (set -q PASSWORD_STORE_DIR; and echo $PASSWORD_STORE_DIR; or echo $HOME/.password-store) -name '*.gpg' | sed 's|.*/\.password-store/||; s|\.gpg||')"
