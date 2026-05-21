4abbaedd75b77fea0f6ef039315fceaecb42f0d6
guychouk <10582405+guychouk@users.noreply.github.com>
2026-05-21 14:43:51 +0200

    bash,bin: fix shellcheck stuff in snip.bash and add gwpr

--- bash/snip.bash
+++ bash/snip.bash
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-# This is an fzf-based snippet selector that inserts into readline, bound to C-s.
+# A snippet selector that inserts into readline, bound to C-s. Depends on fzf.
 _snip_widget() {
-    local snippets_file=~/dotfiles/SNIPPETS
-    local selected=$(
-        awk '/^#/ {desc=substr($0, 3); getline; cmd=$0; print desc "\t" cmd}' "$snippets_file" | \
-        fzf --delimiter='\t' --with-nth=1 \
-            --preview="echo {2..} | bat -l bash -p --color always" \
-            --preview-window=up:3:wrap
+  local selected
+  selected=$(
+    awk '/^#/ {desc=substr($0, 3); getline; cmd=$0; print desc "\t" cmd}' "$SNIPPETS_FILE" | \
+      fzf --delimiter='\t' --with-nth=1 \
+      --preview="echo {2..} | bat -l bash -p --color always" \
+      --preview-window=up:3:wrap
     )
     if [ -n "$selected" ]; then
-        local cmd=$(echo "$selected" | cut -f2-)
-        READLINE_LINE="$cmd"
-        READLINE_POINT=${#READLINE_LINE}
+      local cmd
+      cmd=$(echo "$selected" | cut -f2-)
+      READLINE_LINE="$cmd"
+      READLINE_POINT=${#READLINE_LINE}
     fi
 }
 
