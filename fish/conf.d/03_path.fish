f7af79266b3305c4a60e91d94d66289fe1ebde98
guychouk <10582405+guychouk@users.noreply.github.com>
2026-07-30 16:15:39 +0200

    fish: remove nix and direnv hooks

--- fish/conf.d/03_path.fish
+++ fish/conf.d/03_path.fish
@@ -1,8 +1,5 @@
 # first entry in path is homebrew
 
-# second is nix
-fish_add_path -p "$HOME/.nix-profile/bin"
-fish_add_path -p "/nix/var/nix/profiles/default/bin"
 
 # last are my scripts and binaries in my $HOME directory
 fish_add_path -p "$HOME/bin"
