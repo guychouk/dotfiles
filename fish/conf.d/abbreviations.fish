4bce506584ff6199ba27e26f604150e0d538cca4
guychouk <10582405+guychouk@users.noreply.github.com>
2026-08-06 15:33:34 +0200

    fish: update abbreviations

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -1,3 +1,5 @@
+abbr -a --set-cursor kns 'kubectl config set-context --current --namespace %'
+abbr -a --set-cursor grp 'grep -rn "%" .'
 abbr -a aa       "abduco -a"
 abbr -a ab       "abduco"
 abbr -a ac       "abduco -c"
