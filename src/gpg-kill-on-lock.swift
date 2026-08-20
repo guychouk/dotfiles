71bfeb297c3a6e5216c1c50cb8acce0681f10600
guychouk <10582405+guychouk@users.noreply.github.com>
2026-08-20 09:27:42 +0200

    src: add error reporting in gpg-kill-on-lock daemon

--- src/gpg-kill-on-lock.swift
+++ src/gpg-kill-on-lock.swift
@@ -1,16 +1,28 @@
 import Foundation
 
+func runGpgconf(_ label: String, _ arguments: [String]) {
+    let task = Process()
+    task.launchPath = "/opt/homebrew/bin/gpgconf"
+    task.arguments = arguments
+    do {
+        try task.run()
+    } catch {
+        FileHandle.standardError.write("gpg-kill-on-lock: \(label) failed to launch: \(error)\n".data(using: .utf8)!)
+        return
+    }
+    task.waitUntilExit()
+    if task.terminationStatus != 0 {
+        FileHandle.standardError.write("gpg-kill-on-lock: \(label) exited \(task.terminationStatus)\n".data(using: .utf8)!)
+    }
+}
+
 let center = DistributedNotificationCenter.default()
 center.addObserver(
     forName: NSNotification.Name("com.apple.screenIsLocked"),
     object: nil,
     queue: .main
 ) { _ in
-    let task = Process()
-    task.launchPath = "/opt/homebrew/bin/gpgconf"
-    task.arguments = ["--kill", "gpg-agent"]
-    try? task.run()
-    task.waitUntilExit()
+    runGpgconf("gpgconf --kill gpg-agent", ["--kill", "gpg-agent"])
 }
 
 center.addObserver(
@@ -18,11 +30,7 @@ center.addObserver(
     object: nil,
     queue: .main
 ) { _ in
-    let task = Process()
-    task.launchPath = "/opt/homebrew/bin/gpgconf"
-    task.arguments = ["--launch", "gpg-agent"]
-    try? task.run()
-    task.waitUntilExit()
+    runGpgconf("gpgconf --launch gpg-agent", ["--launch", "gpg-agent"])
 }
 
 RunLoop.main.run()
