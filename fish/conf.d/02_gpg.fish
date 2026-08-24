601f1c015dcc09c4d6cf66fac066d047bdfa79a7
guychouk <10582405+guychouk@users.noreply.github.com>
2026-08-24 10:10:18 +0200

    fish: prune and update

--- fish/conf.d/02_gpg.fish
+++ fish/conf.d/02_gpg.fish
@@ -6,7 +6,6 @@ if command -q gpgconf
   set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket 2>/dev/null)
 end
 
-# auto-switch pinentry mode: curses over SSH, GUI locally
 if test -n "$SSH_TTY"
   pinentry-mode ssh >/dev/null 2>&1
 else
--- fish/conf.d/direnv.fish
+++ fish/conf.d/direnv.fish
@@ -1,5 +1,3 @@
 # Intentionally does not hook direnv.
-#
-# This file shadows homebrew's vendor_conf.d/direnv.fish, which would otherwise install the hook regardless.
-#
+# This file shadows homebrew's vendor_conf.d/direnv.fish and prevents it from running direnv's hook.
 # To re-enable: replace the body with `direnv hook fish | source`.
