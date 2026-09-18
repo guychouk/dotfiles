62d847023537d1498c4594064a2ed8c5a9f2cdac
guychouk <10582405+guychouk@users.noreply.github.com>
2026-09-18 22:48:26 +0200

    fish: use fzf_key_bindings when available, fall back to fzf --fish

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -2,6 +2,10 @@ if command -q fzf
   set -gx FZF_ALT_C_COMMAND ""
   set -gx FZF_DEFAULT_OPTS "--margin=2%,0% --height 70% --info=hidden --layout=reverse --no-scrollbar"
   if status is-interactive
-    fzf --fish | source
+    if functions -q fzf_key_bindings
+      fzf_key_bindings
+    else
+      fzf --fish | source
+    end
   end
 end
